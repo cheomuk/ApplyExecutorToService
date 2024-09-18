8f3b0e71e3dbc1ed7bc68cb6284971b99c4b194a
cheomuk <pkw9815@naver.com>
2024-09-18 22:55:03 +0900

    First Commit

---
+++ src/main/java/com/example/applyexecutortoservice/ApplyExecutorToServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.applyexecutortoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyExecutorToServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyExecutorToServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/applyexecutortoservice/ApplyExecutorToServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applyexecutortoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyExecutorToServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
